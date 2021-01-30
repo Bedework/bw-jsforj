ac6d9f99e13f6bb5282c9787f63750daf09112a8
mike <mikeadouglass@gmail.com>
2021-01-29 22:45:19 -0500

    Add more role definitions

--- src/main/java/org/bedework/jsforj/model/values/JSRoles.java
+++ src/main/java/org/bedework/jsforj/model/values/JSRoles.java
@@ -6,5 +6,10 @@ import org.bedework.jsforj.model.values.collections.JSList;
  * User: mike Date: 1/29/21 Time: 15:38
  */
 public interface JSRoles extends JSList<String> {
+  String roleOwner = "owner";
+  String roleAttendee = "attendee";
+  String roleOptional = "optional";
+  String roleInformational = "informational";
+  String roleChair = "chair";
   String roleContact = "contact";
 }
